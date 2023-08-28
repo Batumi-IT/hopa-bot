3ed4fce491b28d8c7b4f40d53f8de514d9dbfa76
samgozman <sam@gozman.space>
2023-08-28 21:18:45 +0400

    Add simplest bot

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+	"os"
+	"regexp"
+)
+
+func main() {
+	log.Println("Hopa bot started")
+	defer log.Println("Hopa bot stopped")
+
+	token := os.Getenv("TELEGRAM_TOKEN")
+	if token == "" {
+		log.Fatal("TELEGRAM_TOKEN env variable is not set")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Set up updates channel
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	// Listen for messages in group chats
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		message := update.Message.Text
+
+		if message != "" && containsStupidQuestion(message) {
+			reply := tgbotapi.NewMessage(update.Message.Chat.ID, "На рынке Хопа!")
+			_, err := bot.Send(reply)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+}
+
+func containsStupidQuestion(message string) bool {
+	var re = regexp.MustCompile(`где.*купить.*\?|где.*найти.*\?|где.*прода[её]тся.*\?`)
+	return re.MatchString(message)
+}
