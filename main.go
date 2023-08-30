5c0937d0164ca319d03f2b6937c4ff4bc91c34fb
samgozman <sam@gozman.space>
2023-08-30 08:46:44 +0400

    Refactoring

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+type Checks struct {
+	Stupid bool
+	Smart  bool
+}
+
 func main() {
 	log.Println("Hopa bot started")
 	defer log.Println("Hopa bot stopped")
@@ -39,21 +44,29 @@ func main() {
 		}
 
 		message := update.Message.Text
-
-		if message != "" && containsStupidQuestion(message) {
-			reply := tgbotapi.NewMessage(update.Message.Chat.ID, "На рынке Хопа!")
-			reply.ReplyToMessageID = update.Message.MessageID
-			
-			_, err := bot.Send(reply)
-			if err != nil {
-				log.Println(err)
-			}
+		if message == "" {
+			continue
 		}
-		
-		if message != "" && containsSmartQuestion(message) {
-			reply := tgbotapi.NewMessage(update.Message.Chat.ID, "Держи ссылку с адресом рынка Хопа, раз в гугле забанили: https://goo.gl/maps/aqN4rzapdDXvRJNW9")
+
+		check := Checks{
+			Stupid: containsStupidQuestion(message),
+			Smart:  containsSmartQuestion(message),
+		}
+
+		var replyMessage string
+
+		// Note: add switch in case where will be more checks in the future
+		switch check {
+		case Checks{Stupid: false, Smart: false}:
+			replyMessage = "На рынке Хопа!"
+		case Checks{Stupid: true, Smart: false}:
+			replyMessage = "Держи ссылку с адресом рынка Хопа, раз в гугле забанили: https://goo.gl/maps/aqN4rzapdDXvRJNW9"
+		}
+
+		if replyMessage != "" {
+			reply := tgbotapi.NewMessage(update.Message.Chat.ID, replyMessage)
 			reply.ReplyToMessageID = update.Message.MessageID
-			
+
 			_, err := bot.Send(reply)
 			if err != nil {
 				log.Println(err)
